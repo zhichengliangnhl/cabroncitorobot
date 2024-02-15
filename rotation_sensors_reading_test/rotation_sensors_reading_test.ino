6f218dce840ce93be16cf881754b3453ae4ed939
FjodorSmorodins <fjodor.smorodins@student.nhlstenden.com>
2024-02-15 19:49:48 +0100

    I am not gonna test it myself, I am scared of breaking it because I connected rotation sensor pin to IO pins 9 and 10, i want ask Yu-I or somebody else if that should be fine

--- rotation_sensors_reading_test/rotation_sensors_reading_test.ino
+++ rotation_sensors_reading_test/rotation_sensors_reading_test.ino
@@ -2,16 +2,31 @@ int Motor_A1 = 8;  // Left wheel, goes forward
 int Motor_A2 = 6;  // Left wheel, goes backward
 int Motor_B1 = 7;  // Right wheel, goes backward
 int Motor_B2 = 5;  // Right wheel, goes forward
+const int encoderPin_R1 = 9;
+const int encoderPin_R2 = 10// Connect the DO pin of the encoder to digital pin 2
 
+void loop() {
+  int encoderValue = digitalRead(encoderPin);
+  Serial.println(encoderValue); // Print the value to the serial monitor
+  delay(100); // Adjust delay as needed
+}
 void setup() {
   pinMode(Motor_A1, OUTPUT);
   pinMode(Motor_A2, OUTPUT);
   pinMode(Motor_B1, OUTPUT);
   pinMode(Motor_B2, OUTPUT);
+  pinMode(encoderPin, INPUT);
+  pinMode(encoderPin, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
     forward();
+    int encoderValue_R1 = digitalRead(encoderPin_R1);
+    int encoderValue_R2 = digitalRead(encoderPin_R2);
+    Serial.println(encoderValue_R1);
+    Serial.println(encoderValue_R2);// Print the value to the serial monitor
+    delay(200); // Adjust delay as needed
 }
 
 void forward(){
