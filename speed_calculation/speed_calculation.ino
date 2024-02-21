11fd3b517cef3f73ac4b4135db4e6485dcf60976
FjodorSmorodins <fjodor.smorodins@student.nhlstenden.com>
2024-02-21 18:26:15 +0100

    real speed calculation, changed the encode pin setup

--- speed_calculation/speed_calculation.ino
+++ speed_calculation/speed_calculation.ino
@@ -2,16 +2,15 @@ int Motor_A1 = 8;   // Left wheel, goes forward
 int Motor_A2 = 6;   // Left wheel, goes backward
 int Motor_B1 = 7;   // Right wheel, goes backward
 int Motor_B2 = 5;   // Right wheel, goes forward
-const int encoderPin_R2 = 9;
-const int encoderPin_R1 = 10;
+const int encoderPin_R2 = 11;
+const int encoderPin_R1 = 12;
 volatile int prevStateA = HIGH;
 volatile int prevStateB = HIGH;
 volatile int distanceMovedA = 0;
 volatile int distanceMovedB = 0;
 
-unsigned long initialTime;
-int initialDistanceA = 0;
-int initialDistanceB = 0;
+unsigned long lastMillis = 0;
+unsigned long interval = 1000;
 
 void setup() {
   // Initialize encoder input pin and motor control pins
@@ -27,38 +26,29 @@ void setup() {
 void loop() {
   forward();
   
-  // Record initial time and distances
-  initialTime = millis();
-  initialDistanceA = distanceMovedA;
-  initialDistanceB = distanceMovedB;
-
-  // Delay for a short period
-  delay(1000); // Adjust delay time as needed
-  
-  // Get current time
-  unsigned long currentTime = millis();
-
-  // Calculate elapsed time
-  unsigned long elapsedTime = currentTime - initialTime;
-
-  // Calculate distance moved by each wheel
   recordDistancePassedByA();
   recordDistancePassedByB();
-  int distanceMovedWheelA = distanceMovedA - initialDistanceA;
-  int distanceMovedWheelB = distanceMovedB - initialDistanceB;
 
-  // Calculate speeds (distance / time)
-  float speedA = (float) distanceMovedWheelA / elapsedTime;
-  float speedB = (float) distanceMovedWheelB / elapsedTime;
+  unsigned long currentMillis = millis();
+  if (currentMillis - lastMillis >= interval) {
+    // Calculate speed for wheel A
+    float speedA = (float) distanceMovedA / interval; // Speed = Distance / Time
+    Serial.print("Speed of Wheel A: ");
+    Serial.println(speedA, 3);
+    
+    // Calculate speed for wheel B
+    float speedB = (float) distanceMovedB / interval; // Speed = Distance / Time
+    Serial.print("Speed of Wheel B: ");
+    Serial.println(speedB, 3);
 
-  // Print speeds to serial monitor
-  Serial.print("Speed of Wheel A: ");
-  Serial.print(speedA);
-  Serial.println(" units per millisecond");
-  Serial.print("Speed of Wheel B: ");
-  Serial.print(speedB);
-  Serial.println(" units per millisecond");
-  delay(20);
+    // Reset distance counters
+    distanceMovedA = 0;
+    distanceMovedB = 0;
+
+    // Update lastMillis
+    lastMillis = currentMillis;
+  }
+  delay(5);
 }
 
 void recordDistancePassedByA() {
@@ -81,5 +71,5 @@ void forward(){
     analogWrite(Motor_A1, 128);
     analogWrite(Motor_A2, 0);
     analogWrite(Motor_B1, 0);
-    analogWrite(Motor_B2, 254);
+    analogWrite(Motor_B2, 254.9);
 }
